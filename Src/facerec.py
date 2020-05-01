18bae7e9c0ae50a196b49da83b737a26b37d9d17
Andre Da Silva <andre.dasilva@samtrevano.ch>
2020-05-01 17:39:09 +0100

    Creazione Interfaccia grafica di accesso

--- Src/facerec.py
+++ Src/facerec.py
@@ -1,111 +1,240 @@
 # Import required modules
 import cv2 as cv
 import math
-import time
 import argparse
 import mysql.connector
 import os 
+import time
+import datetime
+from tkinter import *
+from PIL import Image, ImageTk
 
 
-#---------- Accesso al database + dichiarazione cursore ----------------------
-mydb = mysql.connector.connect(
-host="localhost",
-user="root",
-passwd="root"
-)
-cursor = mydb.cursor()
-#-----------------------------------------------------------------------------
+def startFaceRecognition():
 
 
-def createDatabase():
+    #---------- Accesso al database + dichiarazione cursore ----------------------
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root"
+    )
+    cursor = mydb.cursor()
 
-    cursor.execute("SHOW DATABASES LIKE 'ScanSpect'")
-    results = 0
-    for x in cursor:
-        results+=1
-    if results == 0:
-        cursor.execute("DROP DATABASE IF EXISTS ScanSpect")
-        cursor.execute("CREATE DATABASE ScanSpect")
+    
+    #------------------------------Resize del frame-------------------------------
+    def rescale_frame(frame, percent=75):
+        width = int(frame.shape[1] * percent/ 100)
+        height = int(frame.shape[0] * percent/ 100)
+        dim = (width, height)
+        return cv.resize(frame, dim, interpolation =cv.INTER_AREA)    
+
+    #-----------------------------------------------------------------------------
+
+
+    #-------------------- Creazione DB + inserimento dati ------------------------
+
+    #crea il database 
+    def createDatabase():
+
+        cursor.execute("SHOW DATABASES LIKE 'ScanSpect'")
+        results = 0
+        for x in cursor:
+            results+=1
+        if results == 0:
+            cursor.execute("CREATE DATABASE ScanSpect")
+            cursor.execute("USE ScanSpect")
+            cursor.execute("CREATE TABLE people(id INT PRIMARY KEY AUTO_INCREMENT,date DATE,hours INT,minutes INT,seconds INT)")
+            mydb.commit()
+
+    #permette l'aggiunta di dati al database (tempo di rivelamento + numero totale di persone)
+    def addDataToDb(date,hours,minutes,secs):
+
+        #Inserimento dati nel Database
+        print("new data added")
         cursor.execute("USE ScanSpect")
-        cursor.execute("DROP TABLE IF EXISTS persone")
-        cursor.execute("CREATE TABLE persone (ora date,persone INTEGER(32))")
+        sql = "INSERT INTO people(date,hours,minutes,seconds) VALUES(%s,%s,%s,%s)"
+        val = (date,hours,minutes,secs)
+        cursor.execute(sql,val)
         mydb.commit()
 
+    #-----------------------------------------------------------------------------   
+        
 
-def addDataToDb(date,nPersons):
+    def getFaceBox(net, frame, conf_threshold=0.7):
+        frameOpencvDnn = frame.copy()
+        frameHeight = frameOpencvDnn.shape[0]
+        frameWidth = frameOpencvDnn.shape[1]
+        blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+        net.setInput(blob)
+        detections = net.forward()
+        bboxes = []
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > conf_threshold:
+                x1 = int(detections[0, 0, i, 3] * frameWidth)
+                y1 = int(detections[0, 0, i, 4] * frameHeight)
+                x2 = int(detections[0, 0, i, 5] * frameWidth)
+                y2 = int(detections[0, 0, i, 6] * frameHeight)
+                bboxes.append([x1, y1, x2, y2])
+                cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (255, 0, 0), int(round(frameHeight/150)), 8)
+        return frameOpencvDnn, bboxes
 
-    #Inserimento dati nel Database
-    cursor.execute("USE ScanSpect")
-    sql = "INSERT INTO persone(ora,persone) VALUES (%s,%s)"
-    val = (date,nPersons)
-    cursor.execute(sql,val)
-    mydb.commit()
-    mydb.close()
+    #------------------------------ Scansione volti ------------------------------
+
+    faceProto = "opencv_face_detector.pbtxt"
+    faceModel = "opencv_face_detector_uint8.pb"
+
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+
+    # Load network
+    faceNet = cv.dnn.readNet(faceModel, faceProto)
+
+    # Open a video file or an image file or a camera stream
+    cap = cv.VideoCapture(0)
+    padding = 20
+    last_face_number = None
+    count = 0
+    createDatabase()
+    while cv.waitKey(1) < 0:
+        # Read frame
+        #t = time.time()
+        hasFrame, frame = cap.read()
+        frame = rescale_frame(frame,percent=100)
+
+        if not hasFrame:
+            cv.waitKey()
+            break
+
+        frameFace, bboxes = getFaceBox(faceNet, frame)
+        face_number = 0
+
+        for bbox in bboxes:
+            # print(bbox)
+            face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+            blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            face_number+=1   
+            cv.imshow("Face detect", frameFace)
+        
+        if last_face_number is not None:
+            if last_face_number < face_number:
+                count+=1
+                now = datetime.datetime.now()
+                addDataToDb(str(datetime.date.today()),int(now.hour),int(now.minute),int(now.second))    
+            last_face_number = face_number
+        else:
+            last_face_number = face_number
+            count+=face_number
+            now = datetime.datetime.now()
+            addDataToDb(str(datetime.date.today()),int(now.hour),int(now.minute),int(now.second))    
+        print(str(count))
+
+    #-----------------------------------------------------------------------------    
+
+    #chiusura della connessione con il database
+    mydb.close()   
+
+#verifica se i dati utente inseriti sono validi
+def testConnection(host,user,password):
     
-
-def getFaceBox(net, frame, conf_threshold=0.7):
-    frameOpencvDnn = frame.copy()
-    frameHeight = frameOpencvDnn.shape[0]
-    frameWidth = frameOpencvDnn.shape[1]
-    blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
-
-    net.setInput(blob)
-    detections = net.forward()
-    bboxes = []
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > conf_threshold:
-            x1 = int(detections[0, 0, i, 3] * frameWidth)
-            y1 = int(detections[0, 0, i, 4] * frameHeight)
-            x2 = int(detections[0, 0, i, 5] * frameWidth)
-            y2 = int(detections[0, 0, i, 6] * frameHeight)
-            bboxes.append([x1, y1, x2, y2])
-            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
-    return frameOpencvDnn, bboxes
+    try:
+        mydbTest = mysql.connector.connect(
+            host=host,
+            user=user,
+            passwd=password
+        )
+        mydbTest.close()
+        return True
+    except (mysql.connector.errors.InterfaceError,mysql.connector.errors.ProgrammingError):
+        return False
 
 
-faceProto = "opencv_face_detector.pbtxt"
-faceModel = "opencv_face_detector_uint8.pb"
+#-------------------------------- Menu Frame in Tkinter ---------------------------------------
 
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+#/////////////////// Frame /////////////////
 
-# Load network
-faceNet = cv.dnn.readNet(faceModel, faceProto)
+#Definisce nuovo frame.
+top = Tk()
+#setta le dimensioni del frame.
+top.geometry("280x320")
+#setta il background bianco.
+top.configure(background='white')
+#aggiunge il titolo al frame.
+top.wm_title("Face recognition")
+#setta a non ridimensionabile il frame.
+top.resizable(width=False, height=False)
 
-# Open a video file or an image file or a camera stream
-cap = cv.VideoCapture(0)
-padding = 20
-last_face_number = None
-count = 0
+#///////////////////////////////////////////
 
-while cv.waitKey(1) < 0:
-    # Read frame
-    #t = time.time()
-    hasFrame, frame = cap.read()
-    if not hasFrame:
-        cv.waitKey()
-        break
 
-    frameFace, bboxes = getFaceBox(faceNet, frame)
-    if not bboxes:
-        print("Nessuna faccia trovata.")
-        continue
-    
-    face_number = 0
-    for bbox in bboxes:
-        # print(bbox)
-        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
-        blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
-        face_number+=1   
-        cv.imshow("Face detect", frameFace)
-       
-    if last_face_number is not None:
-        if last_face_number < face_number:
-            count+=1
-            #addData()
-        last_face_number = face_number
+#//////////////// Logo /////////////////////
+load = Image.open("logo.png")
+render = ImageTk.PhotoImage(load)
+img = Label(top, image=render)
+img.image = render
+img.place(x=30,y=20)
+
+#///////////////////////////////////////////
+
+
+#///////////// Help label //////////////////
+
+L0 = Label(top,text="Insert your Mysql access info:")
+L0.config(font=("Courier",8))
+L0.place(x=30,y=90)
+
+#///////////////////////////////////////////
+
+
+
+#///// Host,user,password label+entry //////
+
+L1 = Label(top, text="Host")
+L1.place(x=30,y=120)
+E1 = Entry(top, bd =5)
+E1.place(x=100,y=120)
+
+L2 = Label(top, text="User")
+L2.place(x=30,y=170)
+E2 = Entry(top, bd =5)
+E2.place(x=100,y=170)
+
+L3 = Label(top, text="Password")
+L3.place(x=30,y=220)
+E3 = Entry(top,show="*",bd =5)
+E3.place(x=100,y=220)
+
+#///////////////////////////////////////////
+
+#In caso venga premuto il tasto start, verifica
+#che i campi non siano vuoti e che i dati inseriti
+#siano validi tramite apposito metodo, se queste
+#condizioni non sono soddisfatte, stampa un label rosso con l'errore.
+def buttonPressed():
+    v = E1.get()
+    v2 = E2.get()
+    v3 = E3.get()
+
+    L4 = Label()
+    if L4.winfo_exists():
+        L4.destroy()
+
+    if len(v) == 0 or len(v2) == 0 or len(v3) == 0:
+       L4 = Label(top, text="I campi vuoti non sono ammessi, riprova.")
+       L4.config(fg="red")
+       L4.place(x=30,y=290)  
+
     else:
-        last_face_number = face_number
-        count+=face_number 
-           
-    print(str(count))  
+        if testConnection(v,v2,v3) is True:
+            top.destroy()
+            startFaceRecognition()
+        else:
+            L4 = Label(top, text="Utente o host inserito non valido, riprova.")
+            L4.config(fg="red")
+            L4.place(x=30,y=290)  
+
+btn = Button(top, text ="Start", width=20,command=buttonPressed)
+btn.place(x=60,y=260)
+top.mainloop()
+
+#----------------------------------------------------------------------------------------
