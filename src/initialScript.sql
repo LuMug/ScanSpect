6530cdbc3b9ea7eab90ddf1af0be3aea478e3445
Nathan Lue <nathan.lue@samtrevano.ch>
2020-05-08 18:43:59 +0200

    Aggiunta script iniziale
    Script che deve venire eseguito dall'utente la prima volta!

---
+++ src/initialScript.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS ScanSpect;
+CREATE DATABASE ScanSpect;
+
+USE ScanSpect;
+
+DROP TABLE IF EXISTS people;
+CREATE TABLE people(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE,
+    hours INT,
+    minutes INT,
+    seconds INT
+);
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user(
+    username varchar(30) PRIMARY KEY, 
+    password varchar(30),
+    admin boolean
+);
+
+DROP USER IF EXISTS 'normalUser'@'localhost';
+CREATE USER 'normalUser'@'localhost' identified by "Normal_1";
+GRANT SELECT, INSERT on ScanSpect.user to 'normalUser'@'localhost';
+GRANT SELECT on ScanSpect.people to 'normalUser'@'localhost';
+
+ALTER USER 'normalUser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Normal_1';
+
+DROP USER IF EXISTS 'adminUser'@'localhost';
+CREATE USER 'adminUser'@'localhost' identified by "!Ciao123";
+GRANT ALL on ScanSpect.* to 'adminUser'@'localhost';
+ALTER USER 'adminUser'@'localhost' IDENTIFIED WITH mysql_native_password BY '!Ciao123';
